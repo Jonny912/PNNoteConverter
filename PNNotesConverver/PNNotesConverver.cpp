66ef5cd974762a128f16ae9d958d8ff489566848
Jonny912 <alexander.petrovskiy@gmail.com>
2021-11-07 18:45:08 +0200

    Preliminary impl

--- PNNotesConverver/PNNotesConverver.cpp
+++ PNNotesConverver/PNNotesConverver.cpp
@@ -2,19 +2,25 @@
 //
 
 #include <iostream>
+#include "pugixml.hpp"
 
 int main()
 {
     std::cout << "Notes Converter for Pacific Network (888poker.com) client (v 0.1 pre-release)\n";
+
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file("Notes.xml");
+    if (!result)
+        return -1;
+
+    //auto notes = doc.children("PlayerNotes");
+    auto notes = doc.document_element();
+    
+    for (auto note : notes)
+    {
+        auto player = doc.children("Note");
+        for (auto attr : player)
+            std::cout << note.attribute("playerName").value() << std::endl;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
